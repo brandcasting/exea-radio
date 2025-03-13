a49bef6574c47af8e88a65e4743dd1e544344e9e
Jeison Jimenez <jjimenez@Mac-mini-de-Jeison.local>
2025-03-13 12:45:10 -0500

    player socker

--- src/player.py
+++ src/player.py
@@ -16,7 +16,7 @@ class Player():
   def __init__(self):
     self.config = Config().getConfig()
     self.lcd = LCD()
-    self.sio = socketio.Client()
+    self.sio = socketio.Client(ssl_verify=False)
     pygame.init()
     pygame.mixer.init()
     pygame.mixer.music.set_endevent(pygame.USEREVENT)
