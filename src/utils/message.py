7ac561d89f28c1b5864ac833bd679f80de7de0cc
Jeison Jimenez <jjimenez@Mac-mini-de-Jeison.local>
2025-04-03 22:38:18 -0500

    update messaging

--- src/utils/message.py
+++ src/utils/message.py
@@ -1,4 +1,5 @@
 import platform
+import time
 
 class Message:
   def __init__(self):
@@ -7,6 +8,7 @@ class Message:
     if not self.linux:
       self.current_row = 0
       self.max_rows = 2  # Suponiendo que tu LCD tiene 2 filas
+      self.max_chars = 16  # Número máximo de caracteres visibles en la pantalla
 
   def getChannel(self):
     if platform.machine() == 'x86_64' or platform.machine() == 'arm64':
@@ -17,20 +19,25 @@ class Message:
       self.linux = False
       return GPIOlibrary()
 
-  def showMessage(self, message):
+  def showMessage(self, message, scroll_speed=0.3):
     if self.linux:
       self.channel.print(message, style="bold green")
     else:
-      truncated_message = self.truncateMessage(message, 16)
-      if self.current_row >= self.max_rows:
-          self.current_row = 0
-          self.channel.clear()
       self.channel.begin(1, 2)
       self.channel.setCursor(0, self.current_row)
-      self.channel.message(truncated_message + "\n")
+      
+      if len(message) <= self.max_chars:
+        # Si el mensaje cabe en la pantalla, se muestra directamente
+        self.channel.message(message.ljust(self.max_chars) + "\n")
+      else:
+        # Si el mensaje es más largo, se desplaza de derecha a izquierda
+        full_message = message + " " * 4  # Espaciado para un mejor bucle
+        for i in range(len(full_message) - self.max_chars + 1):
+          self.channel.setCursor(0, self.current_row)
+          self.channel.message(full_message[i:i + self.max_chars] + "\n")
+          time.sleep(scroll_speed)  # Controla la velocidad del desplazamiento
+
       self.current_row += 1
-  
-  def truncateMessage(self, message, max_length):
-    if len(message) > max_length:
-        return message[:max_length - 3] + '...'
-    return message
+      if self.current_row >= self.max_rows:
+        self.current_row = 0
+        self.channel.clear()
