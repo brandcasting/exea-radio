fb296ad28f568189ea0d785571ade1dd26d9bc90
Jeison Jimenez <jjimenez@Mac-mini-de-Jeison.local>
2025-05-16 10:31:18 -0500

    add fixing response

--- src/player.py
+++ src/player.py
@@ -84,7 +84,7 @@ class Player():
           self.sio.connect(self.config['api'], wait_timeout=5)
           self.sio.on("notification_transmission", functools.partial(self.on_notification_transmission, vlc_player))
         return response['response']
-    except requests.RequestException as e:
+    except Exception:
       return False
     return False
   
