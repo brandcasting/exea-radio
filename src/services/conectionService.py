39aa754aa82711727c5445f006179888293767bc
Jeison Jimenez <jjimenez@Mac-mini-de-Jeison.local>
2026-03-11 17:52:17 -0500

    add conection

--- src/services/conectionService.py
+++ src/services/conectionService.py
@@ -39,13 +39,11 @@ class ConectionService:
   def songByRule(self, id, config):
     user = b64encode(config['user'].encode("ascii"))
     endpoint = config['cms'] + "/player/song/" + id;
-    print(endpoint)
     
     Headers = { 
       "Content-Type" : "application/json",
       "X-USER-TOKEN" : user
     }
-    print(Headers)
     jsonResponse = get(endpoint, headers = Headers)
     response = loads(jsonResponse.text)
     return response
--- src/utils/vlcplayer.py
+++ src/utils/vlcplayer.py
@@ -82,7 +82,6 @@ class VLCPlayer:
       response = self.conection.songByRule(rule['id'], self.config)
       self.current_player.stop()
       song = response['response']['song']
-      print(song)
       media = vlc.Media(song['url'])
       self.current_player.set_media(media)
       self.current_player.play()
