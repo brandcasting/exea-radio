434dbc7dfcab461882136ea9a4299d95828191a0
Jeison Jimenez <jjimenez@Mac-mini-de-Jeison.local>
2026-03-24 12:50:53 -0500

    src player log

--- src/player.py
+++ src/player.py
@@ -86,6 +86,7 @@ class Player():
           self.sio.on("notification_transmission", functools.partial(self.on_notification_transmission, vlc_player))
         return response['response']
     except Exception as e:
+      print(e)
       return False
     return False
   
